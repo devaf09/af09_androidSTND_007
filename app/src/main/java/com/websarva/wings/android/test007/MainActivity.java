2e58248bcd2deb7f6ad66cefecdb659b1e289346
devaf09 <abys.devmg@gmail.com>
2019-01-12 11:03:47 +0900

    007 v001 初期画面、リスナの登録とその試験済み

---
+++ app/src/main/java/com/websarva/wings/android/test007/MainActivity.java
@@ -0,0 +1,43 @@
+package com.websarva.wings.android.test007;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MainListener mainListener = new MainListener();         // リスナ開始
+
+        Button mainbt_receive_start = findViewById(R.id.mainbt_receive_start);
+        mainbt_receive_start.setOnClickListener(mainListener);
+
+
+    }
+
+
+    // クリック関係リスナここ
+    private class MainListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            int objID = view.getId();
+
+            switch (objID) {
+                case R.id.mainbt_receive_start:
+                     Toast.makeText(getApplicationContext(), "mainbt_receive_start : ON", Toast.LENGTH_LONG).show();
+                    break;
+                default:
+                    String mesg = "認識されないオブジェクトがクリックされました。:" + String.valueOf(objID);
+                    Toast.makeText(getApplicationContext(), mesg, Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    }
+    
+}
